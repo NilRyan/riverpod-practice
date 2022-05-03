410d7b0e3f9ec271377213033cb0688df265b0bd
Neil Ryan L <neilryanlipaod@gmail.com>
2022-05-03 20:15:49 +0800

    Initial commit

---
+++ android/app/src/main/kotlin/dev/indifferentcat/riverpod_practice/MainActivity.kt
@@ -0,0 +1,6 @@
+package dev.indifferentcat.riverpod_practice
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Counter App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.green,
+          brightness: Brightness.dark,
+          surface: const Color(0xff003909),
+        )
+      ),
+      home: Container(),
+    );
+  }
+}
+
