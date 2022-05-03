b620143afdf3813a4542ae164abc310960ef1b21
Neil Ryan L <neilryanlipaod@gmail.com>
2022-05-03 20:42:05 +0800

    Implement provider with ConsumerWidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final counterProvider = StateProvider((ref) => 0);
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -13,14 +16,63 @@ class MyApp extends StatelessWidget {
       title: 'Counter App',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.green,
-          brightness: Brightness.dark,
-          surface: const Color(0xff003909),
-        )
-      ),
-      home: Container(),
+          colorScheme: ColorScheme.fromSeed(
+        seedColor: Colors.green,
+        brightness: Brightness.dark,
+        surface: const Color(0xff003909),
+      )),
+      home: const HomePage(),
     );
   }
 }
 
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: const Text('Go to Counter Page'),
+          onPressed: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: ((context) => const CounterPage()),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class CounterPage extends ConsumerWidget {
+  const CounterPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final int counter = ref.watch(counterProvider);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Counter'),
+      ),
+      body: Center(
+        child: Text(
+          counter.toString(),
+          style: Theme.of(context).textTheme.displayMedium,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {
+          ref.read(counterProvider.notifier).state++;
+        },
+      ),
+    );
+  }
+}
